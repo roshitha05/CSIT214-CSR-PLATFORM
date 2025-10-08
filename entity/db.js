cfd9e898a6e1ef4cf26ef08078c91fd7bb688b3a
lly02 <lly.02@outlook.com>
2025-10-08 18:09:52 +0800

    feat: singleton db instance for DI into entity classes

--- entity/db.js
+++ entity/db.js
@@ -3,8 +3,17 @@ import { drizzle } from 'drizzle-orm/neon-serverless';
 import { Pool } from '@neondatabase/serverless';
 
 export default class DB {
+    static instance = null;
+
     constructor() {
         const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-        this.db = drizzle(pool);
+        instance = drizzle(pool);
+    }
+
+    static getInstance() {
+        if (!DB.instance) {
+            DB.instance = new DB();
+        }
+        return DB.instance;
     }
 }
