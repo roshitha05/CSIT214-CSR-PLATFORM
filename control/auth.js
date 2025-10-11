340c9af0f91dd580239f571677c497740504602a
lly02 <lly.02@outlook.com>
2025-10-11 21:37:36 +0800

    feat: POST /login - login by user profile

--- control/auth.js
+++ control/auth.js
@@ -1,3 +1,4 @@
+import z from 'zod';
 import ServerError from '../exception/Error.js';
 import Control from './control.js';
 
@@ -8,32 +9,47 @@ export class Login extends Control {
 
     createController() {
         this.router.post('/login', async (req, res, next) => {
-            const { username, email, password } = req.body ?? {};
-            if ((!username && !email) || !password) {
-                next(
-                    new ServerError(
-                        400,
-                        'Please provide username or email and password'
-                    )
+            const loginSchema = z
+                .object({
+                    username: z.string().optional(),
+                    email: z.string().optional(),
+                    password: z.string(),
+                    user_profile: z.string(),
+                })
+                .refine(
+                    (data) => {
+                        return data.username || data.email;
+                    },
+                    {
+                        message: 'Please provide username or email',
+                    }
                 );
+
+            try {
+                var parsed = loginSchema.parse(req.body);
+            } catch (error) {
+                return next(400, error.issues[0].message);
             }
 
-            if (username !== undefined) {
+            if (parsed.username !== undefined) {
                 var userArray = await this.usersEntity.getUsers({
-                    username,
-                    password,
+                    username: parsed.username,
+                    password: parsed.password,
                 });
             }
-            if (email !== undefined) {
+            if (password !== undefined) {
                 var userArray = await this.usersEntity.getUsers({
-                    email,
-                    password,
+                    email: parsed.email,
+                    password: parsed.password,
                 });
             }
 
             if (userArray.length === 0) {
                 return next(
-                    new ServerError(401, 'Invalid username, email or password')
+                    new ServerError(
+                        401,
+                        'Invalid username, email, password or user profile'
+                    )
                 );
             }
 
@@ -46,9 +62,13 @@ export class Login extends Control {
             // );
             if (!user.password === password) {
                 return next(
-                    new ServerError(401, 'Invalid username, email or password')
+                    new ServerError(
+                        401,
+                        'Invalid username, email, password or user profile'
+                    )
                 );
             }
+
             req.session.regenerate((err) => {
                 if (err) next(new ServerError(500, err));
 
