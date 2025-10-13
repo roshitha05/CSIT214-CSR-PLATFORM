ebe8dfb1f03dd156a599d8b2e71fc1c68b8bbebb
lly02 <lly.02@outlook.com>
2025-10-13 12:00:36 +0800

    feat: single source for all schemas

---
+++ schemas/userProfiles.schema.js
@@ -0,0 +1,22 @@
+import { createInsertSchema } from 'drizzle-zod';
+import z from 'zod';
+import { userProfilesTable } from '../database/tables.js';
+
+export const insertUserProfileSchema = createInsertSchema(userProfilesTable, {
+    name: z.string(),
+    description: z.string(),
+    other: z.string().optional(),
+});
+
+export const searchUserProfilesSchema = z.object({
+    name: z.string().optional(),
+    description: z.string().optional(),
+    status: z.string().optional(),
+    other: z.string().optional()
+})
+
+export const updateUserProfileSchema = z.object({
+    name: z.string().optional(),
+    description: z.string().optional(),
+    other: z.string().optional(),
+})
