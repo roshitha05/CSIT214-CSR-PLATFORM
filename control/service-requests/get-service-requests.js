e97c9a30a4bd22a551c91edc5e21e2b8eeacc01e
lly02 <lly.02@outlook.com>
2025-10-27 14:42:44 +0800

    feat: request management return user

--- control/service-requests/get-service-requests.js
+++ control/service-requests/get-service-requests.js
@@ -9,9 +9,16 @@ export default class GetServiceRequests extends Control {
         this.router.get('/', async (req, res, next) => {
             let serviceRequests = await this.serviceRequestsEntity
                 .getServiceRequests();
-            serviceRequests.map( request => 
-                delete request.view_count
-            );
+            await Promise.all(
+                serviceRequests.map( async serviceRequest => {
+                    delete serviceRequest.view_count
+                    serviceRequest.user = (await this.usersEntity
+                        .getUsers({ 
+                            user_id: serviceRequest.created_by 
+                        }))[0]
+                    }
+                )
+            )
 
             return res.status(200).send(serviceRequests);
         });
