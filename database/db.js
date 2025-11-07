8715e96f42d5cfa041c9e4c34b6a3e239184df86
lly02 <lly.02@outlook.com>
2025-11-07 16:21:57 +0800

    feat:  implement caching

--- database/db.js
+++ database/db.js
@@ -1,13 +1,23 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import { Pool } from '@neondatabase/serverless';
+import { upstashCache } from 'drizzle-orm/cache/upstash';
 
 export default class DB {
     static instance = null;
 
     constructor() {
         const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-        this.db = drizzle(pool);
+        this.db = drizzle(
+            pool,
+            {
+                cache: upstashCache({
+                    url: "https://glowing-monkey-14895.upstash.io",
+                    token: "ATovAAIncDJmZTQ3Nzc0NTljNDA0NTdmYTYzM2IwYjM4M2MwNjA0N3AyMTQ4OTU",
+                    global: true
+                }),
+            }
+        );
         this.pool = pool;
 
         DB.instance = this;
